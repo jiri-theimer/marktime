d1ae0fe4ea2e6531f8c399d6f03814b765b32802
jiri-theimer <jiri.theimer@gmail.com>
2016-10-14 13:01:57 +0200

    backup

--- UI/p31_framework.aspx.vb
+++ UI/p31_framework.aspx.vb
@@ -219,7 +219,7 @@ Public Class p31_framework
             'dataItem("systemcolumn").Text = "<a title='Zapsat úkon' href='javascript:nw(" & cRec.Item("pid").ToString & ")'><img src='Images/new.png' border=0/></a>"
             basUIMT.p41_grid_Handle_ItemDataBound(sender, e, True, False)
             With dataItem("systemcolumn")
-                .Text = "<a class='reczoom' title='Detail projektu' rel='clue_p41_record.aspx?&pid=" & cRec.Item("pid").ToString & "' style='margin-left:-10px;'>i</a>" & .Text
+                .Text = "<a class='reczoom' title='Detail projektu' rel='clue_p41_record_myworksheet.aspx?&pid=" & cRec.Item("pid").ToString & "' style='margin-left:-10px;'>i</a>" & .Text
             End With
         Else
             With dataItem("systemcolumn")
